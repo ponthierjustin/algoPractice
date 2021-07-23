f5b1ee7b16e13b268b3d083d103ee3518fa54c87
Justin <ponthierjustin@gmail.com>
2021-07-22 22:14:51 -0400

    perfect square algo completed

---
+++ perfectSquare.js
@@ -0,0 +1,12 @@
+// Write a function that takes in number and returns whether the number is a perfect square or not
+// Perfect squares are numbers like 4, 49, 81, 100, 144, etc.
+
+function perfectSquare(number){
+    for (i = 1; i < number; i++){
+        if(i * i === number){
+            return true;
+        }
+    }
+    return false;
+}
+console.log(perfectSquare(81));
